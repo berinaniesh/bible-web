8282538b86063c92dc5e73ec3c8833d81ce81fc5
Berin Aniesh <berinaniesh@gmail.com>
2024-02-14 17:52:38 +0530

    start rightmenu

---
+++ src/lib/components/RightMenu.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let isMenuOpen = false
+</script>
+
+
+<div>
+    {#if isMenuOpen}
+        <div class="fixed top-0 right-0 h-screen w-80 bg-red-500">
+            boo
+        </div>
+    {/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import '../app.pcss';
 	import Header from '$lib/components/Header.svelte';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import RightMenu from "$lib/components/RightMenu.svelte";
 </script>
 
 <div class="mx-auto max-w-screen-md px-4 font-sans">
 	<Header isDark={false} />
 	<Separator />
 	<slot />
+	<RightMenu />
 </div>
