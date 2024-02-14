0b173f045126921fbb584da9c927f50aea928458
Berin Aniesh <berinaniesh@gmail.com>
2024-02-14 12:05:14 +0530

    change padding to only x

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
     import Separator from "$lib/components/ui/separator/separator.svelte";
 </script>
 
-<div class="max-w-screen-md mx-auto font-sans p-4">
+<div class="max-w-screen-md mx-auto font-sans px-4">
     <Header isDark={false}/>
     <Separator />
     <slot />
