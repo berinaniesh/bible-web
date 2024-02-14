6ccc17208d605780f04c1ac9c2e283ca0ff0d399
Berin Aniesh <berinaniesh@gmail.com>
2024-02-14 12:36:49 +0530

    remove border from favicons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,7 +12,7 @@
 <div class="my-1 flex h-12 flex-col justify-center">
 	<div class="grid grid-cols-2">
 		<div class="flex">
-			<a href="/"><img class="mr-2 size-10" alt="main-logo" src="/favicon.png" /></a>
+			<a href="/"><img class="mr-2 w-8 h-10" alt="main-logo" src="/favicon.png" /></a>
 			<a href="/" class="flex flex-col justify-center text-2xl font-bold"><p>The Bible</p></a>
 		</div>
 		<div class="flex justify-between text-3xl">
