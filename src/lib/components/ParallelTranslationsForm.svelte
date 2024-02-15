05ff9e498d86bf0e04ee267e68061fb8699af0dc
Berin Aniesh <berinaniesh@gmail.com>
2024-02-15 17:54:04 +0530

    remove repeated code with loop

--- src/lib/components/ParallelTranslationsForm.svelte
+++ src/lib/components/ParallelTranslationsForm.svelte
@@ -4,45 +4,21 @@
     import type { SuperValidated } from "sveltekit-superforms";
     export let form: SuperValidated<ParallelTranslationsFormSchema>;
     export let currentTranslation: string;
+    const translations = ["TOVBSI", "KJV", "MLSVP", "ASV"]
   </script>
 
 
   <Form.Root method="POST" {form} schema={parallelTranslationsFormSchema} let:config>
-    {#if currentTranslation !== "TOVBSI"}
-        <Form.Field {config} name="tovbsi">
-        <Form.Item>
-            <Form.Checkbox />
-            <Form.Label>TOVBSI</Form.Label>
-            <Form.Validation />
-        </Form.Item>
-        </Form.Field>
-    {/if}
-    {#if currentTranslation !== "KJV"}
-        <Form.Field {config} name="kjv">
-            <Form.Item>
-            <Form.Checkbox />
-            <Form.Label>KJV</Form.Label>
-            <Form.Validation />
-            </Form.Item>
-        </Form.Field>
-    {/if}
-    {#if currentTranslation !== "MLSVP"}
-      <Form.Field {config} name="mlsvp">
-        <Form.Item>
-          <Form.Checkbox />
-          <Form.Label>MLSVP</Form.Label>
-          <Form.Validation />
-        </Form.Item>
-      </Form.Field>
-    {/if}
-    {#if currentTranslation !== "ASV"}
-      <Form.Field {config} name="asv">
-        <Form.Item>
-          <Form.Checkbox />
-          <Form.Label>ASV</Form.Label>
-          <Form.Validation />
-        </Form.Item>
-      </Form.Field>
-    {/if}
+    {#each translations as translation}
+        {#if currentTranslation !== translation}
+            <Form.Field {config} name={translation.toLowerCase()}>
+                <Form.Item>
+                    <Form.Checkbox />
+                    <Form.Label>{translation}</Form.Label>
+                    <Form.Validation />
+                </Form.Item>
+            </Form.Field>
+        {/if}
+    {/each}
     <Form.Button>Submit</Form.Button>
   </Form.Root>
