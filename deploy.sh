829d9d4fd1c977d15d4e0e99bbdaa137a9bf20c2
Berin Aniesh <berinaniesh@gmail.com>
2024-02-21 12:49:19 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm run build
-rm bible-web.zip
+rm bible-web.zip 2> /dev/null
 zip -r bible-web.zip .
 scp bible-web.zip berinaniesh.xyz:/home/berinaniesh/tmp/
 ssh berinaniesh.xyz deploy-bible-web.sh
